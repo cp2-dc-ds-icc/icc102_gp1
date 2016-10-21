242935d0f5593d44b019eb11f43788b505b5084d
Gabriele Gomes <gabbygc.bee@gmail.com>
2016-10-21 10:29:16 -0300

    Original
    Esse eh a porra do certo

---
+++ personagens.js
@@ -0,0 +1,924 @@
+Aatrox={
+"Passive":"Poço de Sangue:",
+"Q":"Voo Sombrio:",
+"W":"Sede de Sangue/Preço em Sangue:",
+"E":"Lâminas da Aflição:",
+"R":"Massacre:"
+}
+Ahri={
+"Passive":"Furto de Essência: ",
+"Q":"Orbe da Ilusão:",
+"W":"Fogo de Raposa:",
+"E":"Encanto:",
+"R":"Ímpeto Espiritual:"
+}
+Akali={
+"Passive":"Disciplinas Gêmeas:",
+"Q":"Marca do Assassino:",
+"W":"Proteção do Crepúsculo:",
+"E":"Corte crescente:",
+"R":"Dança das Sombras:"
+}
+Alistar={
+"Passive":"Atropelar:",
+"Q":"Pulverizar:",
+"W":"Cabeçada:",
+"E":"Urro triunfante:",
+"R":"Vontade Indestrutível:"
+}
+Amumu={
+"Passive":"Toque amaldiçoado:",
+"Q":"Lançar bandagens:",
+"W":"Desespero:",
+"E":"Chilique:",
+"R":"Maldição da múmia triste:"
+}
+Anivia={
+"Passive":"Renascimento:",
+"Q":"Lampejo Gelado:",
+"W":"Cristalizar:",
+"E":"Congelamento:",
+"R":"Tempestade Glacial:"
+}
+Annie={
+"Passive":"Piromania:",
+"Q":"Desintegrar:",
+"W":"Incinerar:",
+"E":"Escudo fundido:",
+"R":"Invocar Tibbers:"
+}
+Ashe={
+"Passive":"Tiro Congelado:",
+"Q":"Concentração:",
+"W":"Rajada:",
+"E":"Olhar do Falcão:",
+"R":"Flecha de Cristal Encantada:"
+}
+AurelionSol={
+"Passive":"Centro do Universo:",
+"Q":"Oscilação Estelar:",
+"W":"Expansão Celestial:",
+"E":"Cometa Lendário:",
+"R":"Voz da Luz:"
+}
+Azir={
+"Passive":"Legado de Shurima:",
+"Q":"Areias da Conquista:",
+"W":"Surja!:",
+"E":"Areias Oscilantes:",
+"R":"Decreto do Imperador"
+}
+Bardo={
+"Passive":"Chamado do Viajante:",
+"Q":"Prisão Cósmica:",
+"W":"Santuário do protetor:",
+"E":"Jornada Mágica:",
+"R":"Têmpera do Destino:"
+}
+Blitzcrank={
+"Passive":"Barreira de Mana:",
+"Q":"Puxão biônico:",
+"W":"Turbo:",
+"E":"Punho do Poder:",
+"R":"Campo Estático:"
+}
+Brand={
+"Passive":"Labareda:",
+"Q":"Cauterizar:",
+"W":"Pilar de Chamas:",
+"E":"Conflagração:",
+"R":"Piroclasma:"
+}
+Braum={
+"Passive":"Golpes Concussivos:",
+"Q":"Mordida do Inverno:",
+"W":"Eu te protejo:",
+"E":"Inquebrável:",
+"R":"Fissura Glacial:"
+}
+Caitlyn={
+"Passive":"Bem na mira:",
+"Q":"Pacificadora de Piltover:",
+"W":"Armadilha mecânica Yordle:",
+"E":"Rede Calibre 90:",
+"R":"Ás na Manga:"
+}
+Cassiopeia={
+"Passive":"Graça Serpentina:",
+"Q":"Explosão Venenosa:",
+"W":"Miasma:",
+"E":"Presas Duplas:",
+"R":"Olhar petrificador:"
+}
+Chogath={
+"Passive":"Carnívoro:",
+"Q":"Ruptura:",
+"W":"Grito Selvagem:",
+"E":"Espinhos Vorpais:",
+"R":"Banquete:"
+}
+Corki={
+"Passive":"Munição hextec:",
+"Q":"Bomba de fósforo:",
+"W":"Valquíria:",
+"E":"Metralhadora:",
+"R":"Barragem de Mísseis:"
+}
+Darius={
+"Passive":"Hemorragia:",
+"Q":"Dizimar:",
+"W":"Ataque Mutilador:",
+"E":"Apreender:",
+"R":"Guilhotina de Noxus:"
+}
+Diana={
+"Passive":"Espada de prata lunar:",
+"Q":"Golpe Crescente:",
+"W":"Cascata Lívida:",
+"E":"Colapso Minguante:",
+"R":"Zênite Lunar:"
+}
+DrMundo={
+"Passive":"Injetar Adrenalina:",
+"Q":"Cutelo infectado:",
+"W":"Agonia ardente:",
+"E":"Masoquismo:",
+"R":"Sadismo:"
+}
+Draven={
+"Passive":"League of Draven:",
+"Q":"Revolução do Machado:",
+"W":"Adrenalina:",
+"E":"Sai da frente:",
+"R":"Reta da Morte:"
+}
+Ekko={
+"Passive":"Ressonância Revo-Z:",
+"Q":"Giratempo:",
+"W":"Convergência Paralela:",
+"E":"Mergulho Fásico:",
+"R":"Cronoquebra:"
+}
+Elise={
+"Passive":"Aranha Rainha:",
+"Q":"Neurotoxina/Mordida venenosa:",
+"W":"Cria volátil/Frenesi Aracnídeo:",
+"E":"Casulo/Rapel:",
+"R":"Forma de Aranha:"
+}
+Evelynn={
+"Passive":"Passos sombrios:",
+"Q":"Espinhos de ódio:",
+"W":"Frenesi Negro:",
+"E":"Devastar:",
+"R":"Abraço da Agonia:"
+}
+Ezreal={
+"Passive":"Feitiço do poder crescente:",
+"Q":"Disparo Místico:",
+"W":"Fluxo Essencial:",
+"E":"Translocação Arcana:",
+"R":"Barragem Incendiária:"
+}
+FiddleSticks={
+"Passive":"Pavor:",
+"Q":"Aterrorizar:",
+"W":"Drenar:",
+"E":"Ventania Sombria:",
+"R":"Tempestade de Corvos:"
+}
+Fiora={
+"Passive":"Dança da duelista:",
+"Q":"Estocada:",
+"W":"Ripostar:",
+"E":"Esgrima:",
+"R":"Desafio Grandioso:"
+}
+Fizz={
+"Passive":"Lutador ligeiro:",
+"Q":"Ataque do Ouriço:",
+"W":"Tridente da Pedra do Mar:",
+"E":"Brincalhão/Trapaceiro:",
+"R":"Lançar isca:"
+}
+Galio={
+"Passive":"Pele rúnica:",
+"Q":"Golpe resoluto:",
+"W":"Bastião:",
+"E":"Rajada íntegra:",
+"R":"Ídolo de Durand:"
+}
+Gangplank={
+"Passive":"Julgamento de fogo:",
+"Q":"Negociarrr:",
+"W":"Remover Escorbuto",
+"E":"Barril de Pólvora:",
+"R":"Barragem de Canhão:"
+}
+Garen={
+"Passive":"Perseverança:",
+"Q":"Acerto Decisivo:",
+"W":"Coragem:",
+"E":"Julgamento:",
+"R":"Justiça Demaciana:"
+}
+Gnar={
+"Passive":"Fúria Genética:",
+"Q":"Bumerangue/Pedregulho:",
+"W":"Hiperativo/Safanão:",
+"E":"Salto/Encontrão:",
+"R":"GNAR!:"
+}
+Gragas={
+"Passive":"Happy hour:",
+"Q":"Jogar o Barril:",
+"W":"Fúria da bebedeira:",
+"E":"Barrigada:",
+"R":"Barril explosivo:"
+}
+Graves={
+"Passive":"Nova destino:",
+"Q":"Fim da Linha:",
+"W":"Cortina de Fumaça:",
+"E":"Saque rápido:",
+"R":"Efeito Colateral:"
+}
+Hecarim={
+"Passive":"Caminho da Guerra:",
+"Q":"Enfurecido:",
+"W":"Espírito do Pavor:",
+"E":"Ataque Devastador:",
+"R":"Massacre das Sombras:"
+}
+Heimerdinger={
+"Passive":"Robôs tecmatúrgicos de manutenção:",
+"Q":"Torre evolutiva H-28G:",
+"W":"Micro-mísseis hextec:",
+"E":"Granada de tempestade de elétrons CH-2:",
+"R":"MELHORIA!!!"
+}
+Illaoi={
+"Passive":"Profetisa de um Deus Ancião:",
+"Q":"Golpe de Tentáculo:",
+"W":"Lição Dura:",
+"E":"Teste de Espírito:",
+"R":"Salto de Fé:"
+}
+Irelia={
+"Passive":"Fervor Ioniano:",
+"Q":"Surto da Lâmina:",
+"W":"Estilo Hiten:",
+"E":"Ataque do Equilíbrio:",
+"R":"Lâminas Transcedentais:"
+}
+Janna={
+"Passive":"Brisa de impulso:",
+"Q":"Ventania Uivante:",
+"W":"Zéfiro:",
+"E":"Olho da Tempestade:",
+"R":"Monção:"
+}
+JarvanIV={
+"Passive":"Cadência Marcial:",
+"Q":"Ataque do Dragão:",
+"W":"Égide de Ouro:",
+"E":"Estandarte Demaciano:",
+"R":"Cataclisma:",
+}
+Jax={
+"Passive":"Investida Implacável:",
+"Q":"Salto Atacante:",
+"W":"Energizar:",
+"E":"Contra-Ataque:",
+"R":"Poder do Grão-mestre:"
+}
+Jayce={
+"Passive":"Capacitor Hextec:",
+"Q":"Aos céus!/Disparo chocante:",
+"W":"Campo estático/Hipercarga:",
+"E":"Golpe Trovejante/Portão Acelerador:",
+"R":"Canhão de Mercúrio/Martelo de Mercúrio:"
+}
+Jhin={
+"Passive":"Sussurro:",
+"Q":"Granada Dançante:",
+"W":"Florescer Mortal:",
+"E":"Audiência Cativa:",
+"R":"Aclamação:"
+}
+Jinx={
+"Passive":"Anime-se!:",
+"Q":"Trocando!:",
+"W":"Zap!:",
+"E":"Mordidinha Flamejante!:",
+"R":"Super mega míssil da morte!:"
+}
+Kalista={
+"Passive":"Aprumo Marcial:",
+"Q":"Perfurar:",
+"W":"Vigia:",
+"E":"Lacerar:",
+"R":"Chamado do Destino:"
+}
+Karma={
+"Passive":"Ímpeto Ardente:",
+"Q":"Chama interior:",
+"W":"Decisão Absorta:",
+"E":"Inspiração:",
+"R":"Mantra:"
+}
+Karthus={
+"Passive":"Desafio da Morte:",
+"Q":"Devastar:",
+"W":"Barreira da Dor:",
+"E":"Perverter:",
+"R":"Requiém:"
+}
+Kassadin={
+"Passive":"Pedra do Vazio:",
+"Q":"Esfera Nula:",
+"W":"Lâmina Ínfera:",
+"E":"Força de Pulso:",
+"R":"Caminhar na fenda:"
+}
+Katarina={
+"Passive":"Voracidade:",
+"Q":"Lâmina Ricochete:",
+"W":"Ciclone das Adagas:",
+"E":"Shunpo:",
+"R":"Lótus da Morte:"
+}
+Kayle={
+"Passive":"Fervor Sagrado:",
+"Q":"Acerto de Contas:",
+"W":"Bênção Divina:",
+"E":"Fúria Íntegra:",
+"R":"Intervenção:"
+}
+Kennen={
+"Passive":"Marca da Tormenta:",
+"Q":"Shuriken Trovejante:",
+"W":"Surto Elétrico:",
+"E":"Investida Relâmpago:",
+"R":"Turbilhão Cortante:"
+}
+KhaZix={
+"Passive":"Ameaça Invísivel:",
+"Q":"Sabor de medo:",
+"W":"Espinho do Vazio:",
+"E":"Salto:",
+"R":"Massacre do Vazio:"
+}
+Kindred={
+"Passive":"Marca Familiar:",
+"Q":"Dança de Flechas:",
+"W":"Frenesi do Lobo:",
+"E":"Pesar Crescente:",
+"R":"Refúgio da Ovelha:"
+}
+Kled={
+"Passive":"Skaarl, a Lagarto Covarde:",
+"Q":"Armadilha na corda:",
+"W":"Tendências Violentas:",
+"E":"Justar:",
+"R":"Avançaaaaaaar!!!:"
+}
+KogMaw={
+"Passive":"Surpresa Icathiana:",
+"Q":"Cusparada Cáustica:",
+"W":"Barragem Bio-arcana:",
+"E":"Gosma do Vazio:",
+"R":"Artilharia Viva:"
+}
+LeBlanc={
+"Passive":"Imagem espelho:",
+"Q":"Sigilo de Malícia:",
+"W":"Distorção:",
+"E":"Correntes Etéreas:",
+"R":"Mímica:"
+}
+LeeSin={
+"Passive":"Agitação:",
+"Q":"Onda Sônica/Ataque Ressonante:",
+"W":"Proteger/Vontade de Ferro:",
+"E":"Tempestade/Mutilar:",
+"R":"Fúria do Dragão:",
+}
+Leona={
+"Passive":"Luz do Sol:",
+"Q":"Proteção da Aurora:",
+"W":"Eclipse:",
+"E":"Lâmina Zênite:",
+"R":"Labareda Solar:"
+}
+Lissandra={
+"Passive":"Glacinata:",
+"Q":"Estilhaço de gelo:",
+"W":"Circulo Ártico:",
+"E":"Caminho Glacial:",
+"R":"Túmulo Congelado:"
+}
+Lucian={
+"Passive":"Disparo Iluminado:",
+"Q":"Luz Perfurante:",
+"W":"Chama Ardente:",
+"E":"Perseguição Implacável:",
+"R":"O Expurgo:"
+}
+Lulu={
+"Passive":"Pix, a fada companheira:",
+"Q":"Lança Purpurina:",
+"W":"Caprichos:",
+"E":"Socorro,Pix!:",
+"R":"Crescimento Virente:"
+}
+Lux={
+"Passive":"Iluminação:",
+"Q":"Ligação da Luz:",
+"W":"Barreira Prismática:",
+"E":"Singularidade Lucente:",
+"R":"Centelha Final:"
+}
+Malphite={
+"Passive":"Escudo de Granito:",
+"Q":"Fragmento Sísmico:",
+"W":"Golpes Brutais:",
+"E":"Estrondar Terreno:",
+"R":"Força Incontrolável:"
+}
+Malzahar={
+"Passive":"Oscilação do Vazio:",
+"Q":"Chamado do Vazio:",
+"W":"Enxame do Vazio:",
+"E":"Visões Maléficas:",
+"R":"Aperto Ínfero:"
+}
+Maokai={
+"Passive":"Seiva Mágica:",
+"Q":"Esmagamento Arcano:",
+"W":"Avanço Retorcido:",
+"E":"Atirar Mudas:",
+"R":"Turbilhão Vingativo:"
+}
+MasterYi={
+"Passive":"Ataque Duplo:",
+"Q":"Ataque Alpha:",
+"W":"Meditar:",
+"E":"Estilo Wuju:",
+"R":"Highlander:"
+}
+MissFortune={
+"Passive":"Batida do Amor:",
+"Q":"Dois por Um:",
+"W":"Desfilando:",
+"E":"Chuva de Disparos:",
+"R":"Metendo Bala:"
+}
+Mordekaiser={
+"Passive":"Iron Man:",
+"Q":"Mace of Spades:",
+"W":"Harverster of Sorrow:",
+"E":"Siphon of Destruction:",
+"R":"Children of the Grave:"
+}
+Morgana={
+"Passive":"Sifão da Alma:",
+"Q":"Ligação das Trevas:",
+"W":"Solo Atormentado:",
+"E":"Escudo Negro:",
+"R":"Grilhões da Alma:"
+}
+Nami={
+"Passive":"Maré Oscilante:",
+"Q":"Prisão Aquática:",
+"W":"Vazante e Fluxo:",
+"E":"Bênção da Conjuradora:",
+"R":"Maré Violenta:"
+}
+Nasus={
+"Passive":"Devorador de Almas:",
+"Q":"Ataque Sifão:",
+"W":"Murchar:",
+"E":"Fogo Espiritual:",
+"R":"Fúria das Areias:"
+}
+Nautilus={
+"Passive":"Âncora Impactante:",
+"Q":"Lançar Âncora:",
+"W":"Ira do Titã:",
+"E":"Correnteza:",
+"R":"Carga de Profundidade:"
+}
+Nidalee={
+"Passive":"Espreitar:",
+"Q":"Arremessar Lança/Bote:",
+"W":"Arapuca/Investida:",
+"E":"Ímpeto Selvagem/Patada:",
+"R":"Aspecto do Puma:"
+}
+Nocturne={
+"Passive":"Lâminas Sombrias:",
+"Q":"Portador do Anoitecer:",
+"W":"Proteção das Trevas:",
+"E":"Horror Indescritível:",
+"R":"Paranoia:"
+}
+Nunu={
+"Passive":"Visionário:",
+"Q":"Consumir:",
+"W":"Ferver o Sangue:",
+"E":"Explosão de Gelo:",
+"R":"Zero Absoluto:"
+}
+Olaf={
+"Passive":"Fúria Berserker:",
+"Q":"Ressaca:",
+"W":"Golpes Nefastos:",
+"E":"Balanço Temerário:",
+"R":"Ragnarok:"
+}
+Orianna={
+"Passive":"Dando corda:",
+"Q":"Comando:Atacar:",
+"W":"Comando:Dissonância:",
+"E":"Comando:Proteger:",
+"R":"Comando:Onda de Choque:"
+}
+Pantheon={
+"Passive":"Proteção de Égide:",
+"Q":"Atirar Lança:",
+"W":"Égide de Zeonia:",
+"E":"Ataque Busca-coração:",
+"R":"Grande Estocada Aérea:"
+}
+Poppy={
+"Passive":"Embaixadora de Ferro:",
+"Q":"Choque de Martelo:",
+"W":"Presença Inabalável:",
+"E":"Investida Heróica:",
+"R":"Veredito da Guardiã:"
+}
+Quinn={
+"Passive":"Rapina:",
+"Q":"Investida Anuviante:",
+"W":"Sentidos Apurados:",
+"E":"Salto:",
+"R":"Retaguarda do Inimigo:"
+}
+Rammus={
+"Passive":"Casco espetado:",
+"Q":"Bola do Poder:",
+"W":"Bola Curva defensiva:",
+"E":"Provocação Perfurante:",
+"R":"Tremores:"
+}
+RekSai={
+"Passive":"Fúria dos Xer'Sai:",
+"Q":"Ira da Rainha/Sondar presas:",
+"W":"Escavar/Emergir:",
+"E":"Mordida Feroz/Túnel:",
+"R":"Investida do Vazio:"
+}
+Renekton={
+"Passive":"Domínio da Ira:",
+"Q":"Abater os indefesos:",
+"W":"Predador desumano:",
+"E":"Fatiar e picar:",
+"R":"Dominus:"
+}
+Rengar={
+"Passive":"Predador Oculto:",
+"Q":"Selvageria:",
+"W":"Rugido de Batalha:",
+"E":"Boleadeiras:",
+"R":"Furor da Caçada:"
+}
+Riven={
+"Passive":"Lâmina Rúnica:",
+"Q":"Asas Quebradas:",
+"W":"Explosão de Ki:",
+"E":"Valentia:",
+"R":"Lâmina do Exílio:"
+}
+Rumble={
+"Passive":"Titã do Ferro velho:",
+"Q":"Cospe-Fogo:",
+"W":"Escudo de Sucata:",
+"E":"Arpão Elétrico:",
+"R":"O Equalizador:"
+}
+Ryze={
+"Passive":"Maestria Arcana:",
+"Q":"Sobrecarregar:",
+"W":"Prisão de Runa:",
+"E":"Fluxo de Feitiço:",
+"R":"Portal de Reinos:"
+}
+Sejuani={
+"Passive":"Congelar:",
+"Q":"Ataque do Ártico:",
+"W":"Mangual dos ventos boreais:",
+"E":"Congelação Permanente:",
+"R":"Prisão Glacial:"
+}
+Shaco={
+"Passive":"Apunhalar:",
+"Q":"Enganar:",
+"W":"Caixinha surpresa:",
+"E":"Veneno de dois gumes:",
+"R":"Alucinações:"
+}
+Shen={
+"Passive":"Barreira de Ki:",
+"Q":"Ataque Crepúsculo:",
+"W":"Refúgio Espiritual:",
+"E":"Corrida das Sombras:",
+"R":"Manter a União:"
+}
+Shyvana={
+"Passive":"Meio-Dragão:",
+"Q":"Mordida dupla:",
+"W":"Combustão:",
+"E":"Hálito Flamejante:",
+"R":"Descida do Dragão:"
+}
+Singed={
+"Passive":"Bastião do poder:",
+"Q":"Rastro de veneno:",
+"W":"Mega adesivo:",
+"E":"Lançar:",
+"R":"Poção da Insanidade:"
+}
+Sion={
+"Passive":"In gloria mori:",
+"Q":"Golpe demolidor:",
+"W":"Fornalha da Alma:",
+"E":"Urro do Assassino:",
+"R":"Investida Incontrolável:"
+}
+Sivir={
+"Passive":"Pés ligeiros:",
+"Q":"Lâmina Bumerangue:",
+"W":"Ricochete:",
+"E":"Escudo de Feitiço:",
+"R":"Na caçada:"
+}
+Skarner={
+"Passive":"Espirais Cristalinas:",
+"Q":"Corte de Cristal:",
+"W":"Exoesqueleto Cristalino:",
+"E":"Fratura:",
+"R":"Empalar:"
+}
+Sona={
+"Passive":"Power Chord:",
+"Q":"Hino do Valor:",
+"W":"Ária da perseverança:",
+"E":"Canção da Celeridade:",
+"R":"Crescendo:"
+}
+Soraka={
+"Passive":"Salvação:",
+"Q":"Chamado Estelar:",
+"W":"Infusão Astral:",
+"E":"Equinócio:",
+"R":"Desejo "
+}
+Swain={
+"Passive":"Renovação Pútrida:",
+"Q":"Decreptar:",
+"W":"Nunca movam:",
+"E":"Tormenta:",
+"R":"Bando voraz:"
+}
+Syndra={
+"Passive": "Transcender:",
+"Q": "Esfera negra:",
+"W":"Determinação:",
+"E":"Dispersar os fracos:",
+"R":"Poder Irrestrito:"
+}
+Tahmkench={
+"Passive": "Um gosto adquirido:",
+"Q": "Língua-chicote:",
+"W":"Devorar:",
+"E":"Pele grossa:",
+"R":"Viagem Abissal:"
+}
+Taliyah={
+"Passive": "Deslizar em pedras:",
+"Q": "Voleio entrelaçado:",
+"W":"Empurrão Sísmico:",
+"E":"Terra desfiada:",
+"R":"Muro de Tecelão:"
+}
+Talon={
+"Passive": "Piedade:",
+"Q": "Diplomacia Noxiana:",
+"W":"Ancinho:",
+"E":"Degolar:",
+"R":"Ataque das Sombras:"
+}
+Taric={
+"Passive": "Bravata:",
+"Q": "Fugor Estelar:",
+"W":"Bastião:",
+"E":"Deslumbrar:",
+"R":"Resplendor Cósmico:"
+}
+Teemo={
+"Passive": "Camuflagem:",
+"Q": "Dardo Ofuscante:",
+"W":"Mover depressa:",
+"E":"Tiro tóxico:",
+"R":"Armadilha venenosa:"
+}
+Thresh={
+"Passive": "Condenação:",
+"Q": "Sentença:",
+"W":"Passagem sombria:",
+"E":"Esfolar:",
+"R":"A Caixa:",
+}
+Tristana={
+"Passive": "Tiro Certeiro:",
+"Q": "Tiro rápido:",
+"W":"Salto foguete:",
+"E":"Carga explosiva:",
+"R":"Tiro destruidor:"
+}
+Trundle={
+"Passive": "Tributo do Rei:",
+"Q": "Mordida:",
+"W":"Domínio congelado:",
+"E":"Pilar de Gelo:",
+"R":"Subjugar:"
+}
+Tryndamere={
+"Passive": "Fúria da Batalha:",
+"Q": "Sanguinário:",
+"W":"Grito Zombador:",
+"E":"Corte giratório:",
+"R":"Fúria sem fim:"
+}
+TwistedFate={
+"Passive": "Dados viciados:",
+"Q": "Coringas:",
+"W":"Escolha uma carta:",
+"E":"Baralho marcado:",
+"R":"Destino:"
+}
+Twitch={
+"Passive": "Veneno mortal:",
+"Q": "Emboscada:",
+"W":"Tonel de veneno:",
+"E":"Contaminar:",
+"R":"Ra-ta-ta-ta"
+}
+Udyr={
+"Passive": "Agilidade do macaco:",
+"Q": "Postura do tigre:",
+"W":"Postura da tartaruga:",
+"E":"Postura do Urso:",
+"R":"Postura da fênix:"
+}
+Urgot={
+"Passive": "Aumentador de raios tocado por Zaun:",
+"Q": "Caçador Ácido:",
+"W":"Condensador do Terror:",
+"E":"Carga corrosiva noxiana:",
+"R":"Reversor de posição hipercinético:"
+}
+Varus={
+"Passive": "Vingança Viva:",
+"Q": "Flecha perfurante:",
+"W":"Aljava da Ruína:",
+"E":"Chuva de Flechas:",
+"R":"Corrente da corrupção:"
+}
+Vayne={
+"Passive": "Caçadora noturna:",
+"Q": "Rolamento:",
+"W":"Dardos de prata:",
+"E":"Condenar:",
+"R":"Hora Final:"
+}
+Veigar={
+"Passive": "Poder maligno fenomenal:",
+"Q": "Golpe maligno:",
+"W":"Matéria negra:",
+"E":"Horizonte de Eventos:",
+"R":"Explosão Primordial:"
+}
+Velkoz={
+"Passive": "Desconstrução da Orgânica:",
+"Q": "Fissão Plasmática:",
+"W":"Fenda do Vazio:",
+"E":"Ruptura Tectônica:",
+"R":"Raio desintegrador de formas de vida:"
+}
+Vi={
+"Passive": "Blindagem:",
+"Q": "Quebra-Cofres:",
+"W":"Pancada Certeira:",
+"E":"Força Excessiva:",
+"R":"Saque e enterrada:"
+}
+Viktor={
+"Passive": "Evolução Gloriosa:",
+"Q": "Poder do Sifão:",
+"W":"Campo Gravítico:",
+"E":"Raio da Morte:",
+"R":"Tempestade do Caos:"
+}
+Vladimir={
+"Passive": "Pacto Vermelho:",
+"Q": "Transfusão:",
+"W":"Poça de Sangue:",
+"E":"Maré de Sangue:",
+"R":"Hemopraga:"
+}
+Volibear={
+"Passive": "O Escolhido da tormenta:",
+"Q": "Trovão Giratório:",
+"W":"Frenesi:",
+"E":"Rugido Majestoso:",
+"R":"Garras do Trovão:"
+}
+Warwick={
+"Passive": "Sede eterna:",
+"Q": "Ataque sedento:",
+"W":"Chamado dos caçadores:",
+"E":"Aroma do Sangue:",
+"R":"Coerção Infinita:"
+}
+Wukong={
+"Passive": "Pele de Pedra:",
+"Q": "Golpe destruidor:",
+"W":"Chamariz:",
+"E":"Resplendor das nuvens:",
+"R":"Ciclone:"
+}
+Xerath={
+"Passive": "Oscilação de Mana:",
+"Q": "Pulso Arcano:",
+"W":"Olho da destruição:",
+"E":"Orbe eletrizante:",
+"R":"Ritual Arcano:"
+}
+XinZhao={
+"Passive": "Desafio:",
+"Q": "Golpe de três garras:",
+"W":"Grito de guerra:",
+"E":"Investida Audaciosa:",
+"R":"Varredura Crescente:"
+}
+Yasuo={
+"Passive": "Estilo do Errante:",
+"Q": "Tempestade de Aço:",
+"W":"Parede de Vento:",
+"E":"Espada Ágil:",
+"R":"Último Sussurro:"
+}
+Yorick={
+"Passive": "Pastor de Almas:",
+"Q": "Extrema-Unção:",
+"W":"Procissão Sombria:",
+"E":"Névoa dos Lamentos:",
+"R":"Louvor das Ilhas:"
+}
+Zac={
+"Passive": "Divisão Celular:",
+"Q": "Esticada:",
+"W":"Matéria Instável:",
+"E":"Estilingue Elástico:",
+"R":"Vamos pular!:"
+}
+Zed={
+"Passive": "Desprezo pelos fracos:",
+"Q": "Shuriken Laminado:",
+"W":"Sombra Viva:",
+"E":"Corte Sombrio:",
+"R":"Marca Fatal:"
+}
+Ziggs={
+"Passive": "Pavio Curto:",
+"Q": "Bomba saltitante:",
+"W":"Carga concentrada:",
+"E":"Campo minado dos hexplosivos:",
+"R":"Bomba Mega Infernal:"
+}
+Zilean={
+"Passive": "Tempo Engarrafagado:",
+"Q": "Bomba Relógio:",
+"W":"Retroceder:",
+"E":"Distorção no Tempo:",
+"R":"Alteração Temporal:"
+}
+Zyra={
+"Passive": "Jardim de Espinhos:",
+"Q": "Farpas mortais:",
+"W":"Crescimento desenfreado:",
+"E":"Pântano das Raízes:",
+"R":"Espinhos Sufocantes:"
+}
